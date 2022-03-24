bfc003fe12a75e22d418e9e60d88c2d118fb2479
KhrystinaRusak <71019455+KhrystinaRusak@users.noreply.github.com>
2022-03-24 14:06:05 +0300

    create script1.js

---
+++ script.js
@@ -0,0 +1,26 @@
+ // create function currentSum
+ // method reduce
+ const numbers = [27, 03, 12, 55];
+ let value = numbers.reduce((sum, current) => sum + current, 0);
+
+ // create a function for returning from the first pieces of this string
+ let str = "To be or not to be";
+ array.filter(function(0, str), 0);
+
+ // create a function changeArray for changing a half of arrays
+ //Создать функцию changeArray, которая будет менять местами половины массивов, 
+ //т.е. если количество элементов четное, то вторая половина становится сначала массива,
+ // а первая на место второй; если количество элементов нечетное, тогда элемент массива, 
+ // который является серединой остается на месте,
+ //а половины меняются местами.
+ const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 'bird'];
+
+ function changeArray() {
+     let firstPiece = animals.slice(0, 2);
+     let secondPiece = animals.slice(3, 5);
+     if (array.half.length % 2 == 0) {
+         animals.copyWithin();
+     } else {
+         animals.copyWithin();
+     };
+ }
